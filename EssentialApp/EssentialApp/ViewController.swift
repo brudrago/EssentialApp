627f7973931a75abf54f7b930d273f81e46e8be8
Bruna Drago <bruna.drago@ifood.com.br>
2023-07-10 18:44:43 -0300

    [Essential]Create new ios project to be the main app

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by bruna.drago on 10/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
