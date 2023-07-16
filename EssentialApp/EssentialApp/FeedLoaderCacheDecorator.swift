ce55ae29fe2486502dd212bced5c04463338f65d
Bruna Drago <bruna.drago@ifood.com.br>
2023-07-16 18:18:31 -0300

    [Essential] Create saveIgnoringResult

--- EssentialApp/EssentialApp/FeedLoaderCacheDecorator.swift
+++ EssentialApp/EssentialApp/FeedLoaderCacheDecorator.swift
@@ -16,9 +16,15 @@ public final class FeedLoaderCacheDecorator: FeedLoader {
             //                self?.cache.save(feed) {_ in}
             //            }other way, instead of using if statement
             completion(result.map { feed in
-                self?.cache.save(feed) {_ in}
+                self?.cache.saveIgnoringResult(feed)
                 return feed
             })
         }
+    }
+}
+
+private extension FeedCache {
+    func saveIgnoringResult(_ feed: [FeedImage]) {
+        save(feed) {_ in}
     }
 }
