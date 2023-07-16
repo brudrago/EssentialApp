097a2459e61d198e2fd5f47b54a950dab4b599d0
Bruna Drago <bruna.drago@ifood.com.br>
2023-07-16 18:11:23 -0300

    [Essential]Move FeedCache to production code, and make LocalFeedLoader to implement FeedCache

--- EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
@@ -3,12 +3,6 @@ import Essential_Feed
 
 @testable import EssentialApp
 
-protocol FeedCache {
-    typealias Result = Swift.Result<Void, Error>
-    
-    func save(_ feed:[FeedImage], completion: @escaping (Result) -> Void)
-}
-
 final class FeedLoaderCacheDecorator: FeedLoader {
     private let decoratee: FeedLoader
     private let cache: FeedCache
