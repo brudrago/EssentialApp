f261e8ae9ce4ec3b7b0b432fbb3bd48023c676a7
Bruna Drago <bruna.drago@ifood.com.br>
2023-07-19 10:42:05 -0300

    [Essential]Add #if DEBUG compilation directive to prevent test-specif code from being deployed in production

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -4,41 +4,41 @@ import Essential_Feed
 import EssentialFeediOS
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
         
-//        let url =  "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed"
-//        let urlString = URL(string: url)!
-//        let session = URLSession(configuration: .ephemeral)
-//        let remoteClient = URLSessionHTTPClient(session: session)
-//        let remoteFeedLoader = RemoteFeedLoader(client: remoteClient, url: urlString)
-//        let remoteImageDataLoader = RemoteFeedImageDataLoader(client: remoteClient)
-//
-//        let localStoreURL = NSPersistentContainer
-//            .defaultDirectoryURL()
-//            .appendingPathComponent("feed-store.sqlite")
-//
-//        let localStore = try! CoreDataFeedStore(storeURL: localStoreURL)
-//        let localFeedLoader = LocalFeedLoader(store: localStore, currentDate: Date.init)
-//        let localImageLoader = LocalFeedImageDataLoader(store: localStore)
-//
-//        window?.rootViewController = FeedUIComposer.feedComposeWith(
-//            feedLoader: FeedLoaderWithFallbackComposite(
-//                primary: FeedLoaderCacheDecorator(decoratee: remoteFeedLoader, cache: localFeedLoader),
-//                fallback: localFeedLoader
-//            ),
-//            imageLoader: FeedImageDataLoaderWithFallbackComposite(
-//                primary: localImageLoader,
-//                fallback: FeedImageDataLoaderCacheDecorator(
-//                    decoratee: remoteImageDataLoader,
-//                    cache: localImageLoader
-//                )
-//            )
-//        )
+        //        let url =  "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed"
+        //        let urlString = URL(string: url)!
+        //        let session = URLSession(configuration: .ephemeral)
+        //        let remoteClient = URLSessionHTTPClient(session: session)
+        //        let remoteFeedLoader = RemoteFeedLoader(client: remoteClient, url: urlString)
+        //        let remoteImageDataLoader = RemoteFeedImageDataLoader(client: remoteClient)
+        //
+        //        let localStoreURL = NSPersistentContainer
+        //            .defaultDirectoryURL()
+        //            .appendingPathComponent("feed-store.sqlite")
+        //
+        //        let localStore = try! CoreDataFeedStore(storeURL: localStoreURL)
+        //        let localFeedLoader = LocalFeedLoader(store: localStore, currentDate: Date.init)
+        //        let localImageLoader = LocalFeedImageDataLoader(store: localStore)
+        //
+        //        window?.rootViewController = FeedUIComposer.feedComposeWith(
+        //            feedLoader: FeedLoaderWithFallbackComposite(
+        //                primary: FeedLoaderCacheDecorator(decoratee: remoteFeedLoader, cache: localFeedLoader),
+        //                fallback: localFeedLoader
+        //            ),
+        //            imageLoader: FeedImageDataLoaderWithFallbackComposite(
+        //                primary: localImageLoader,
+        //                fallback: FeedImageDataLoaderCacheDecorator(
+        //                    decoratee: remoteImageDataLoader,
+        //                    cache: localImageLoader
+        //                )
+        //            )
+        //        )
         
         let remoteURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
         let remoteClient = makeRemoteClient()
@@ -50,16 +50,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             .defaultDirectoryURL()
             .appendingPathComponent("feed-store.sqlite")
         
-        #if DEBUG
+#if DEBUG
         if CommandLine.arguments.contains("-reset") {
             try? FileManager.default.removeItem(at: localStoreURL)
         }
-        #endif
+#endif
         
         let localStore = try! CoreDataFeedStore(storeURL: localStoreURL)
         let localFeedLoader = LocalFeedLoader(store: localStore, currentDate: Date.init)
         let localImageLoader = LocalFeedImageDataLoader(store: localStore)
-
+        
         window?.rootViewController = FeedUIComposer.feedComposeWith(
             feedLoader: FeedLoaderWithFallbackComposite(
                 primary: FeedLoaderCacheDecorator(
@@ -77,23 +77,25 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             )
         )
         //To Test if the local loaders are working
-//        window?.rootViewController = FeedUIComposer.feedComposeWith(
-//            feedLoader: localFeedLoader,
-//            imageLoader: localImageLoader
-//        )
+        //        window?.rootViewController = FeedUIComposer.feedComposeWith(
+        //            feedLoader: localFeedLoader,
+        //            imageLoader: localImageLoader
+        //        )
     }
     
     
     private func makeRemoteClient() -> HTTPClient {
-        switch UserDefaults.standard.string(forKey: "connectivity") {
-        case "offline":
+#if DEBUG
+        if UserDefaults.standard.string(forKey: "connectivity")  == "offline" {
             return AlwaysFailingHTTPClient()
-            
-        default:
-            return URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
         }
+#endif
+        
+        return URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
     }
+}
     
+#if DEBUG
     private class AlwaysFailingHTTPClient: HTTPClient {
         private class Task: HTTPClientTask {
             func cancel() {}
@@ -104,5 +106,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             return Task()
         }
     }
-}
-
+#endif
